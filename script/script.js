39c3570830dcec1fdb4baca1ca89a3f8d8fe896d
Suman Shekhar <suman.sumanshekhar698@gmail.com>
2023-08-18 18:52:59 +0530

    updated code

--- script/script.js
+++ script/script.js
@@ -38,3 +38,44 @@ const widthSize = window.matchMedia("(max-width: 780px)");
 myFunction(widthSize);
 // Attach listener function on state changes
 widthSize.addListener(myFunction);
+
+
+
+// Clients Counter
+
+$.fn.jQuerySimpleCounter = function( options ) {
+  var settings = $.extend({
+      start:  0,
+      end:    100,
+      easing: 'swing',
+      duration: 400,
+      complete: ''
+  }, options );
+
+  var thisElement = $(this);
+
+  $({count: settings.start}).animate({count: settings.end}, {
+  duration: settings.duration,
+  easing: settings.easing,
+  step: function() {
+    var mathCount = Math.ceil(this.count);
+    thisElement.text(mathCount);
+  },
+  complete: settings.complete
+});
+};
+
+
+$('#number1').jQuerySimpleCounter({end: 18,duration: 3000});
+$('#number2').jQuerySimpleCounter({end: 22,duration: 3000});
+$('#number3').jQuerySimpleCounter({end: 8,duration: 2000});
+$('#number4').jQuerySimpleCounter({end: 6,duration: 2500});
+
+
+
+/* AUTHOR LINK */
+ $('.about-me-img').hover(function(){
+        $('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
+    }, function(){
+        $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
+    });
