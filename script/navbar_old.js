1ede76019ee4617342709ab3ef02c58f4a315d00
Suman Shekhar <suman.sumanshekhar698@gmail.com>
2023-09-25 20:35:48 +0530

    updated media

--- script/navbar_old.js
+++
@@ -1,8 +0,0 @@
-const headerEl = document.querySelector('.navbar');
-
-const isScrolling = () => {
-	const windowPosition = window.scrollY > 50
-	headerEl.classList.toggle('navbar--active', windowPosition)
-}
-
-window.addEventListener('scroll', isScrolling)
--- script/script.js
+++ script/script.js
@@ -1,6 +1,32 @@
 // ========================================
 //  how to add media queries in JS
 // ========================================
+
+/*===== Progress Bar =====*/ 
+const progressBarIndicator = document.querySelector("#progress");
+const whatsappOpenner = document.querySelector("#whatsapp_opener");
+
+document.addEventListener("scroll", () => {
+  let pos = document.documentElement.scrollTop;
+//   console.log(pos);
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  // console.log(calcHeight);
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  console.log(scrollValue);
+
+  if(scrollValue > 11){
+    whatsappOpenner.style.display = "grid";
+  } else {
+    whatsappOpenner.style.display = "none";
+  }
+
+  progressBarIndicator.style.width = scrollValue + "%";
+});
+
+
+/*===== TESTIMONIAL SWIPER =====*/ 
 function testimonialsSwiperFunction(widthSize) {
   if (widthSize.matches) {
     // If media query matches
